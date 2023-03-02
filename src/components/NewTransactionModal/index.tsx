34a552295758c2f9ddd5f098615a1f5aba6e9d1f
Eduardo Freitas <eduardo.freitas@cscezos.com.br>
2023-03-02 19:09:41 -0300

    create hook useTransactions :tada:

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
-import { FormEvent, useState, useContext } from "react";
+import { FormEvent, useState } from "react";
+import { useTransactions } from "../../hooks/useTransactions";
 import Modal from "react-modal";
-import { TransactionsContext } from "../../TransactionsContext";
 
 import income from "../../assets/income.svg";
 import outcome from "../../assets/outcome.svg";
@@ -17,7 +17,7 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext)
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,16 +1,13 @@
-import { useContext } from 'react'
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import income from "../../assets/income.svg";
 import outcome from "../../assets/outcome.svg";
 import total from "../../assets/total.svg";
 
-
-
 import { Container } from "./styles";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
+  const { transactions } = useTransactions();
 
   const summary = transactions.reduce((acc, transaction) => {
     if (transaction.type === 'deposit') {
